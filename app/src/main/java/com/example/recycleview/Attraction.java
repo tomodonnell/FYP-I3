c279134fee8dd67b1c8779271a6bf37ce50523d8
tomodonnell <117367303@umail.ucc.ie>
2021-02-01 13:54:09 +0000

    Iteration 3, fourth commit, minor changes to province.

--- app/src/main/java/com/example/recycleview/Attraction.java
+++ app/src/main/java/com/example/recycleview/Attraction.java
@@ -30,7 +30,7 @@ public class Attraction extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_munster);
 
         createAttractionList();
         buildRecyclerView();
--- app/src/main/java/com/example/recycleview/Cork.java
+++ app/src/main/java/com/example/recycleview/Cork.java
@@ -23,7 +23,7 @@ public class Cork extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_munster);
 
         createCorkList();
         buildRecyclerView();
