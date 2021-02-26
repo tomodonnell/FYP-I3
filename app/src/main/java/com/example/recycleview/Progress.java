91a103c9455b83450885b3a23e5734637de823d4
tomodonnell <117367303@umail.ucc.ie>
2021-02-26 20:05:55 +0000

    Iteration 5.1, first commit, user progress bar and progress percentage save button working and complete

--- app/src/main/java/com/example/recycleview/Progress.java
+++ app/src/main/java/com/example/recycleview/Progress.java
@@ -2,31 +2,22 @@ package com.example.recycleview;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 //This Java Class creates an array list and builds the RecyclerView, enabling the implementation the RecyclerView feature.
 //Based on "Coding in FLow" Youtube lectures, however the code has been manipulated and styled to suit my solution.
@@ -43,21 +34,26 @@ public class Progress extends AppCompatActivity {
     public CheckBox lismoreBox;
     public Button save_attraction;
 
-    FirebaseDatabase database;
-    DatabaseReference reference;
-    Member update;
-    int i = 0;
+    //Variables for our shared preferences
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String PROGRESS_PERCENTAGE_AS_STRING = "text";
+    public static final String GAR = "gar";
+    public static final String LOUGH = "lough";
+    public static final String DON = "don";
+    public static final String LISMORE = "lismore";
 
+    //Variables to display the shared preferences after reopening the app.
+    public String stored_progress_percentage_value;
+    public boolean stored_gar_value;
+    public boolean stored_lough_value;
+    public boolean stored_don_value;
+    public boolean stored_lismore_value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
 
-        reference = database.getInstance().getReference().child("viewed");
-        update = new Member();
-
-        progressStatus = 0;
         progressBar = findViewById(R.id.progress_bar);
         percent = findViewById(R.id.txtpercent);
         garBox = findViewById(R.id.GarBox);
@@ -66,81 +62,23 @@ public class Progress extends AppCompatActivity {
         lismoreBox = findViewById(R.id.LismoreBox);
         save_attraction = findViewById(R.id.save);
 
-        garBox.setChecked(Update("CHECK_1"));
-        loughBox.setChecked(Update("CHECK_2"));
-        donBox.setChecked(Update("CHECK_3"));
-        lismoreBox.setChecked(Update("CHECK_4"));
-
-        String a1 = "Garnish";
-        String a2 = "Lough";
-        String a3 = "Don";
-        String a4 = "Lismore";
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    i = (int) dataSnapshot.getChildrenCount();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
         save_attraction.setOnClickListener(new View.OnClickListener() {
             @Override
-
             public void onClick(View v) {
-                if (garBox.isChecked()) {
-                    update.setAttraction1(a1);
-                    reference.child(String.valueOf(i + 1)).setValue(update);
-                    SaveChecked("CHECK_1", garBox.isChecked());
-                } else {
-                    SaveChecked("CHECK_1", false);
-                }
-
-                if (loughBox.isChecked()) {
-                    update.setAttraction2(a2);
-                    reference.child(String.valueOf(i + 1)).setValue(update);
-                    SaveChecked("CHECK_2", loughBox.isChecked());
-
-                } else {
-                    SaveChecked("CHECK_2", false);
-
-                }
-
-                if (donBox.isChecked()) {
-                    update.setAttraction3(a3);
-                    reference.child(String.valueOf(i + 1)).setValue(update);
-                    SaveChecked("CHECK_3", donBox.isChecked());
-                } else {
-                    SaveChecked("CHECK_3", false);
-
-                }
-
-                if (lismoreBox.isChecked()) {
-                    update.setAttraction4(a4);
-                    reference.child(String.valueOf(i + 1)).setValue(update);
-                    SaveChecked("CHECK_4", lismoreBox.isChecked());
-                } else {
-                    SaveChecked("CHECK_4", false);
-                }
+                saveData();
+                Toast.makeText(getBaseContext(), "Your Progress has been Updated!" , Toast.LENGTH_SHORT ).show();
             }
-
-
         });
 
-
         garBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (garBox.isChecked()) {
                     progressStatus += 25;
                     progressBar.setProgress(progressStatus);
                     percent.setText(progressStatus + "%");
+
 
                 } else {
                     progressStatus -= 25;
@@ -206,18 +144,68 @@ public class Progress extends AppCompatActivity {
             }
         });
 
+        //Reloading user data on start up
+        loadData();
+        updateTheScreenWhenAppReOpens();
+
     }
 
-    private void SaveChecked(String key, boolean value) {
-        SharedPreferences sp = getSharedPreferences("SAVE_CHECK", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putBoolean(key, value);
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //putting value of the textview and checkboxes into the shared preferences
+        editor.putString(PROGRESS_PERCENTAGE_AS_STRING, percent.getText().toString());
+        editor.putBoolean(GAR, garBox.isChecked());
+        editor.putBoolean(LOUGH, loughBox.isChecked());
+        editor.putBoolean(DON, donBox.isChecked());
+        editor.putBoolean(LISMORE, lismoreBox.isChecked());
+
         editor.apply();
     }
 
-    private boolean Update(String key) {
-        SharedPreferences sp = getSharedPreferences("SAVE_CHECK", MODE_PRIVATE);
-        return sp.getBoolean(key, false);
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
+        //Assign the stored values to these variables
+        stored_progress_percentage_value = sharedPreferences.getString(PROGRESS_PERCENTAGE_AS_STRING, "0");
+        stored_gar_value = sharedPreferences.getBoolean(GAR, false);
+        stored_lough_value = sharedPreferences.getBoolean(LOUGH, false);
+        stored_don_value = sharedPreferences.getBoolean(DON, false);
+        stored_lismore_value = sharedPreferences.getBoolean(LISMORE, false);
+
     }
 
+    public void updateTheScreenWhenAppReOpens(){
+
+        //Assign the shared preference values (from above in loadData) to the actual controls on the GUI
+        percent.setText(stored_progress_percentage_value);
+        garBox.setChecked(stored_gar_value);
+        loughBox.setChecked(stored_lough_value);
+        donBox.setChecked(stored_don_value);
+        lismoreBox.setChecked(stored_lismore_value);
+    }
+
+    //Saving data when the app gets closed
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadData();
+        updateTheScreenWhenAppReOpens();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveData();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveData();
+    }
 }
+
+
